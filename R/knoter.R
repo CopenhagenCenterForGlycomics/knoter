370d12b9bc5771bf0df15fc7adec4eb4fcb0dcb4
Hiren Joshi <hirenj@gmail.com>
2015-05-25 21:39:06 +0200

    Fix for email addresses

--- R/knoter.R
+++ R/knoter.R
@@ -3,6 +3,6 @@
 #' In order to produce output that is compatible with OneNote APIs,
 #'
 #' @name Knoter
-#' @author Hiren Joshi <knoter@glycocode.com>
+#' @author Hiren Joshi <\email{knoter@@glycocode.com}>
 #' @docType package
 NULL
