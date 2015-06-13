895a1c8fb861e1c551dece945b96838cb7d51197
Hiren Joshi <hirenj@gmail.com>
2015-06-13 20:30:09 +0200

    Tests for output formats

--- tests/testthat/test-markdown.R
+++ tests/testthat/test-markdown.R
@@ -1,7 +1,13 @@
 context("knitting markdown")
 
 test_that("knoter can detect markdown", {
-  test_result = 'markdown-simple.html'
-  test_expected = readChar(test_result, file.info(test_result)$size)
-  expect_equal( knoter::knit( text='# Header \n Inline R `r 1+1`\n',append.meta.created=F ), test_expected )
+    test_result = 'markdown-simple_output.html'
+    test_expected = readChar(test_result, file.info(test_result)$size)
+    expect_equal( knoter::knit( text='# Header \n Inline R `r 1+1`\n',append.meta.created=F ), test_expected )
+})
+
+test_that("knoter can convert html", {
+    test_result = 'html-simple_output.html'
+    test_expected = readChar(test_result, file.info(test_result)$size)
+    expect_equal( knoter::knit( 'html-simple.Rhtml' ,append.meta.created=F ), test_expected )    
 })
